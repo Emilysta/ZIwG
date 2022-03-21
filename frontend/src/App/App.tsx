d9b13acd5ae79248e76c24d8fdf6087989a609ba
Górka Mateusz <maatiug@gmail.com>
2022-03-21 09:19:32 +0100

    main header menu

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,14 +1,52 @@
 import logo from '../images/logo.svg';
+import '../Assets/general.css'
 import './App.css';
+import './HeadBar.css'
 import * as React from "react";
 
 type AppProps = { num: number };
 
-//export const App = ({ num }: AppProps) => <h1>Hello world React! Num: {num}</h1>;
+class Logo extends React.Component {
+  render(): React.ReactNode {
+    return <img src={logo} className="Logo" />
+  }
+}
+
+class Logotype extends React.Component {
+  render(): React.ReactNode {
+    return <div className='Logotype'>
+      <Logo />
+      <a>EventColab</a>
+    </div>
+  }
+}
+
+class TopMenu extends React.Component {
+  render(): React.ReactNode {
+    const list = ["Marisa", "Julianne", "Jakob"]
+    return <li className='TopMenu'>
+      {list.map((name, i) => {
+        return <ul key={i}><a>{name}</a></ul>
+      })}
+    </li>
+  }
+}
+
+class HeadBar extends React.Component {
+  render() {
+    return <div className='HeadBar'>
+      <nav>
+        <Logotype />
+        <TopMenu />
+      </nav>
+    </div>
+  }
+}
 
 function App({ num }: AppProps) {
   return (
     <div className="App">
+      <HeadBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
