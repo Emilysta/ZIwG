869ef5c67cfb053e71bc314985f08d5926b5d2ec
Górka Mateusz <maatiug@gmail.com>
2022-03-21 08:21:39 +0100

    Reset react code & polish css of component scratch

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -6,101 +6,24 @@ type AppProps = { num: number };
 
 //export const App = ({ num }: AppProps) => <h1>Hello world React! Num: {num}</h1>;
 
-function Test(props: any) {
-  return (
-    <h1>Elo! {(new Date().toTimeString())}</h1>
-  )
-}
-
-interface IClockProps {
-}
-
-interface IClockState {
-  date?: Date,
-}
-
-class Clock extends React.Component<IClockProps, IClockState> {
-  constructor(props: any) {
-    super(props);
-    this.state = { date: new Date() };
-  }
-
-  componentDidMount(): void {
-    setInterval(() => this.setState({ date: new Date() }), 1000)
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Witaj, świecie!</h1>
-        <h2>Aktualny czas: {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
-}
-
-interface ICatProps {
-  mouse: any
-}
-
-interface ICatStates {
-}
-
-class Cat extends React.Component<ICatProps, ICatStates> {
-  render() {
-    const mouse = this.props.mouse;
-    return (
-      <img src="/cat.jpg" style={{ position: 'absolute', left: mouse.x, top: mouse.y }} />
-    );
-  }
-}
-
-class MouseWithCat extends React.Component {
-  constructor(props: any) {
-    super(props);
-    this.handleMouseMove = this.handleMouseMove.bind(this);
-    this.state = { x: 0, y: 0 };
-  }
-
-  handleMouseMove(event: any) {
-    this.setState({
-      x: event.clientX,
-      y: event.clientY
-    });
-  }
-
-  render() {
-    return (
-      <div style={{ height: '100vh' }} onMouseMove={this.handleMouseMove}>
-        {/*
-          Moglibyśmy w tym miejscu po prostu zamienić <p> na <Cat>... ale później
-          musielibyśmy stworzyć oddzielny komponent <MouseWithSomethingElse>
-          dla każdego przypadku użycia, dlatego też <MouseWithCat>
-          nie jest jeszcze w pełni używalny wielokrotnie.
-        */}
-        <Cat mouse={this.state} />
-      </div>
-    );
-  }
-}
-
-class MouseTracker extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Porusz myszką!</h1>
-        <MouseWithCat />
-      </div>
-    );
-  }
-}
-
-
-
-
 function App({ num }: AppProps) {
   return (
-      <MouseTracker />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
