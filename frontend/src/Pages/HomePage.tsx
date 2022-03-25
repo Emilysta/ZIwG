d9c394a84d39e73cc757f648640bed2f212e23cf
Górka Mateusz <maatiug@gmail.com>
2022-03-25 16:37:44 +0100

    HomePage content

--- frontend/src/Pages/HomePage.tsx
+++ frontend/src/Pages/HomePage.tsx
@@ -1,8 +1,30 @@
 import * as React from 'react'
+import './HomePage.css'
+import peopleImage from '../images/pexels-tima-miroshnichenko-6615107_edited.png'
 
 function HomePage() {
     return (
-        <div>HomePage</div>
+        <div className='horizontalContainer'>
+            <section className="leftContainer">
+                <div>
+                    <h1 className='highlighted title'>Find Intreating Events</h1>
+                    <div className='content'>
+                        <span>
+                            <p>Tempore quae molestias aliquid voluptas <br />animi ut aut asperiores minima.</p>
+                        </span>
+                        <span>
+                            <input type="button" value="See Events" />
+                        </span>
+                    </div>
+                </div>
+            </section>
+
+            <section className="rightContainer">
+                <div>
+                    <img id="peopleImage" src={peopleImage} />
+                </div>
+            </section>
+        </div>
     )
 }
 
