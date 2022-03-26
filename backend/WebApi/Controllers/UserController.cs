b41a47b8f6da6c7c904b2899e8309fe5fadb80d7
Górka Mateusz <maatiug@gmail.com>
2022-03-26 13:03:16 +0100

    fixed swagger - reverse /api path to endpoint

--- backend/WebApi/Controllers/UserController.cs
+++ backend/WebApi/Controllers/UserController.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace WebApi.Controllers
 {
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
     {
