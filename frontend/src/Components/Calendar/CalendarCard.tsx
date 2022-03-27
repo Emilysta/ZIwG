84432bdc0af1d6c47ac784e5f96fbb302c2b4f09
Emilia Starczyk <emiliastarczyk@gmail.com>
2022-03-27 13:59:55 +0200

    CalendarCard, index.css - change in defining of colors

---
+++ frontend/src/Components/Calendar/CalendarCard.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import './CalendarCard.css';
+
+export default function CalendarCard() {
+    return (
+        <div className='calendarCardBase cardSelected'>CalendarCard</div>
+    )
+}
--- frontend/src/Pages/UserPage.tsx
+++ frontend/src/Pages/UserPage.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import UserCalendarPage from './UserCalendarPage'
 import UserDataPage from './UserDataPage'
 import UserTicketsPage from './UserTicketsPage'
+import './UserPage.css'
 
 export default function UserPage() {
     return (
