47c1324d90aa2082469af9a9b861ecccb952f071
Górka Mateusz <maatiug@gmail.com>
2022-03-28 00:13:44 +0200

    stats panel added, css polish

--- frontend/src/Pages/ProfileStats.tsx
+++ frontend/src/Pages/ProfileStats.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
+import './ProfileStats.scss'
 
 export function ProfileStats(props: any) {
+    const stats = [
+        { name: "voluptatem", num: 15 },
+        { name: "molestiae", num: 1 },
+        { name: "magnam", num: 4 }
+    ]
     return (
         <>
-            <div>
-                <div>
-                    <a>earum rem dolores</a>
-                    <a>15</a>
-                </div>
+            <div className='statsContainer'>
+                {stats.map(stat => <div key={stat.name}>
+                    <label>{stat.name}</label>
+                    <a>{stat.num}</a>
+                </div>)}
             </div>
         </>
     );
