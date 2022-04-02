da8e0d938e306f91bbf2081eea0b78cf8d55e17c
Górka Mateusz <maatiug@gmail.com>
2022-04-02 21:09:11 +0200

    Handle error messeng on submit

--- frontend/src/Components/RegisterForm.tsx
+++ frontend/src/Components/RegisterForm.tsx
@@ -8,15 +8,17 @@ import { Divider } from "./Divider";
 import { validLogin, validEmail, passwordValidate } from "Utils/TextInputValidation";
 
 export function RegisterForm() {
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        console.log("submit");
-        event.preventDefault();
-    };
-
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [errorMsg, setErrorMsg] = useState('')
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        console.log("submit");
+        event.preventDefault();
+        validateAndSend();
+    }
 
     const checkConfirm = (inputValue: string) => {
         return password.length !== 0 && confirmPassword.length !== 0 && password !== confirmPassword
@@ -31,6 +33,7 @@ export function RegisterForm() {
             <TextInput placeHolder='Password' overrideType="password" onChange={(e) => setConfirmPassword(e.target.value)} validate={passwordValidate} />
             <TextInput placeHolder='Confirm password' overrideType="password" onChange={(e) => setPassword(e.target.value)} validate={[checkConfirm]} />
             <StateButton type="submit" value="Create account" />
+            {errorMsg.length > 0 && <p className='inputError errorBox'>{errorMsg}</p>}
         </form>
     );
 
@@ -43,4 +46,37 @@ export function RegisterForm() {
             <p className="text2">Already have an account?<Link to='/logIn' className='highlighted'> Login Here</Link></p>
         </section>
     );
+
+    function validateAndSend() {
+        // sendRequest();
+    }
+
+    function sendRequest() {
+        const body = JSON.stringify({
+            firstName: "string",
+            lastName: "string",
+            password: "string",
+            displayName: "string",
+            dateOfBirth: "2022-04-02T18:22:55.829Z",
+            email: "string"
+        });
+
+        fetch('/api/user/register', {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: body,
+        }).then(data => {
+            if (data.ok) {
+                // todo handle registration
+            }
+            else {
+                setErrorMsg("Error");
+            }
+        }).catch(() => {
+            setErrorMsg("Communication error");
+        });
+    }
 }
