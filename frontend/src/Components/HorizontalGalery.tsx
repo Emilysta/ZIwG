b1fee253b77fdaf1f8834ff8c1d451f26d735d74
Górka Mateusz <maatiug@gmail.com>
2022-04-10 18:48:18 +0200

    good looking horizontal galery

--- frontend/src/Components/HorizontalGalery.tsx
+++ frontend/src/Components/HorizontalGalery.tsx
@@ -8,13 +8,25 @@ export function HorizontalGalery(props: any) {
             <div className='galeryFade'></div>
             <div className='galeryContainer'>
                 <div className='galeryRows'>
-                    <div className='box'>
-                        <Image src="https://images.unsplash.com/photo-1517353334933-3365be5c8ec3" />
-                        <Image src="https://images.unsplash.com/photo-1568993703320-07e80bc8e7ab" />
-                        <Image src="https://images.unsplash.com/photo-1529392960549-df4af50eac23" />
-                        <Image src="https://images.unsplash.com/photo-1518972559570-7cc1309f3229" />
-                        <Image src="https://images.unsplash.com/photo-1583897251569-657efb696b21" />
-                        <Image src="https://images.unsplash.com/photo-1534840641466-b1cdb8fb155e" />
+                    <div className='row'>
+                        <div className='box'>
+                            <Image src="https://images.unsplash.com/photo-1517353334933-3365be5c8ec3" />
+                            <Image src="https://images.unsplash.com/photo-1568993703320-07e80bc8e7ab" />
+                            <Image src="https://images.unsplash.com/photo-1529392960549-df4af50eac23" />
+                            <Image src="https://images.unsplash.com/photo-1518972559570-7cc1309f3229" />
+                            <Image src="https://images.unsplash.com/photo-1583897251569-657efb696b21" />
+                            <Image src="https://images.unsplash.com/photo-1534840641466-b1cdb8fb155e" />
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='box'>
+                            <Image src="https://images.unsplash.com/photo-1583897251569-657efb696b21" />
+                            <Image src="https://images.unsplash.com/photo-1534840641466-b1cdb8fb155e" />
+                            <Image src="https://images.unsplash.com/photo-1529392960549-df4af50eac23" />
+                            <Image src="https://images.unsplash.com/photo-1518972559570-7cc1309f3229" />
+                            <Image src="https://images.unsplash.com/photo-1568993703320-07e80bc8e7ab" />
+                            <Image src="https://images.unsplash.com/photo-1517353334933-3365be5c8ec3" />
+                        </div>
                     </div>
                 </div>
             </div>
