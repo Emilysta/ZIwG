65df12fbb8cfded4d8df4eb3a2132dfb7505b10a
Arkadiusz Kacperski <kacperski.arkadiusz1@gmail.com>
2022-05-09 13:44:59 +0200

    endpoint for adding main image to event

--- backend/Application/Interfaces/IEventService.cs
+++ backend/Application/Interfaces/IEventService.cs
@@ -2,6 +2,8 @@
 using Application.DTOs.EventDTOs;
 using System;
 using System.Collections.Generic;
+using Application.DTOs.UserDTOs;
+
 namespace Application.Interfaces
 {
     public interface IEventService
@@ -11,6 +13,7 @@ namespace Application.Interfaces
         public Task<ReturnEventExtendedDTO> GetEventById(int id);
         public bool ModifyEvent(ModifyEventDTO @event, int id);
         public Task<List<ReturnEventsAsListDTO>> GetEvents(string location, string monthAndYear, string userId);
+        public Task<bool> UploadMainImage(FileUpload fileObj, int eventId);
         public Task<bool> SaveChangesAsync();
     }
 }
