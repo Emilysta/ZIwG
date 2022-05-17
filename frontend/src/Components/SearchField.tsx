6c8350704c0b942cf93ce4cf4dc81d3ce01e01d2
Górka Mateusz <maatiug@gmail.com>
2022-05-17 20:48:28 +0200

    Searching events on event page

--- frontend/src/Components/SearchField.tsx
+++ frontend/src/Components/SearchField.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
-import { Search } from 'react-bootstrap-icons';
+import { Search, X } from 'react-bootstrap-icons';
 import './SearchField.scss'
+
+type SearchResult = (value: string, filter: (value: string) => boolean) => void
 
 type SearchFieldProps = {
     dictionary: string[],
     className?: string,
     maxSuggestions?: number,
-    onChosen?: (value: string) => any
+    onChosen?: SearchResult
 };
 
 class SearchSuggestionsState {
@@ -16,39 +18,54 @@ class SearchSuggestionsState {
     get first(): string { return this.suggestions[0] }
 
     constructor(props: SearchFieldProps, search: string) {
-        this.regex = new RegExp(search, 'ig')
-        this.suggestions = props.dictionary.filter((e) => e.match(this.regex))
+        let filter = this.createNewFilter(search)
+        this.suggestions = props.dictionary.filter(filter)
 
         if (props.maxSuggestions)
             this.suggestions = this.suggestions.filter((_, i) => i < props.maxSuggestions)
     }
+
+    createNewFilter(word: string): (value: string) => boolean {
+        this.regex = new RegExp(word, 'ig')
+        return this.getFilter()
+    }
+
+    getFilter(): (value: string) => boolean {
+        return this.regex
+            ? (value) => value.match(this.regex) != null
+            : (_) => true
+    }
 }
 
 export function SearchField(props: SearchFieldProps) {
-    const [search, setSearch]: [string, any] = React.useState(null)
+    const [search, setSearch]: [string, any] = React.useState('')
     const [suggestions, setSuggestions]: [SearchSuggestionsState, any] = React.useState(new SearchSuggestionsState(props, search))
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.currentTarget.value)
+    const handleKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => e.key === 'Enter' && onChosen()
+    const handleClear = () => setSearch('');
 
-    const handleKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (props.onChosen && e.key === 'Enter') props.onChosen(suggestions.first)
+    const onChosen = () => {
+        if (props.onChosen)
+            props.onChosen(search, suggestions.getFilter());
     }
 
     React.useEffect(() => setSuggestions(new SearchSuggestionsState(props, search)), [search])
+    React.useEffect(() => onChosen(), [suggestions])
 
     const expandStyle: React.CSSProperties = { display: search ? 'block' : 'none' }
 
     return (
         <span className={`searchField ${props.className}`}>
-            <input type="text" placeholder='Search' defaultValue={search} onChange={handleChange} onKeyUp={handleKeyUp} />
-            <Search />
+            <input type="text" placeholder='Search' value={search} onChange={handleChange} onKeyUp={handleKeyUp} />
+            {search ? <X onClick={handleClear} /> : <Search />}
             {
                 <div className='expandable' style={expandStyle}>
                     <ul className='eventPageSearchSuggestions'>
                         {suggestions.suggestions.map((word, i) => {
                             const start: number = word.search(suggestions.regex)
                             const finish: number = start + search.length;
-                            return <li key={i} onClick={(e) => props.onChosen && props.onChosen(word)}>
+                            return <li key={i} onClick={(e) => props.onChosen && props.onChosen(word, suggestions.createNewFilter(word))}>
                                 {word.substring(0, start)}
                                 <b>{word.substring(start, finish)}</b>
                                 {word.substring(finish)}
--- frontend/src/Pages/EventsPage.tsx
+++ frontend/src/Pages/EventsPage.tsx
@@ -4,75 +4,48 @@ import EventTile from 'Components/EventTile';
 import TagList from 'Components/EventPage/TagList';
 import { SearchField } from 'Components/SearchField';
 import { useGetEventsQuery } from 'Utils/EventAPISlice';
+import { EventDataSimple } from 'Utils/EventData';
 
-const mockWordList = [
-    "Amet",
-    "deleniti",
-    "maxime",
-    "quisquam",
-    "Libero",
-    "laborum",
-    "architecto",
-    "sint",
-    "quia",
-    "excepturi",
-    "aliquam",
-    "Voluptatibus",
-    "sapiente",
-    "corrupti",
-    "vitae",
-    "Quis",
-    "consequuntur",
-    "autem",
-    "aperiam",
-]
 
 export default function EventsPage() {
     const { data, error, isLoading } = useGetEventsQuery();
     //const events = data.map((e, i) => <EventTile key={i} data={e} />);
-    const onSearch = (search: string) => alert(search)
+    const [events, setEvents]: [EventDataSimple[], any] = React.useState([...Array(10)])
+    const [search, setSearch]: [string[], any] = React.useState([])
+
+    const onSearch = (search: string, filter: (v: string) => boolean) => {
+        if (data)
+            setEvents(data.filter((e) => filter(e.name)))
+    }
     const tagChosen = (value: string, id: number) => alert(value + " id: " + id)
+
+    React.useEffect(() => {
+        if (!isLoading) {
+            setSearch(data.map((e) => e.name))
+            setEvents(data)
+        }
+    }, [isLoading])
 
     if (error)
         return (<> <p> Oh no, there was an error </p></>);
-    else if (isLoading)
-        return (
-            <>
-                <div className='wholePageLayout navbarMargin'>
-                    <div className='eventsPage'>
-                        <div className='eventsNav'>
-                            <div>
-                                <TagList tags={[{ name: "Concert" }, { name: "Workshops" }, { name: "Conference" }, { name: "xyz" }, { name: "abc" }, { name: "123" }]} isReadOnly={true} onClick={tagChosen} />
-                            </div>
-                            <div>
-                                <SearchField dictionary={mockWordList} maxSuggestions={6} onChosen={onSearch} />
-                            </div>
+
+    return (
+        <>
+            <div className='wholePageLayout navbarMargin'>
+                <div className='eventsPage'>
+                    <div className='eventsNav'>
+                        <div>
+                            <TagList tags={[{ name: "Concert" }, { name: "Workshops" }, { name: "Conference" }, { name: "xyz" }, { name: "abc" }, { name: "123" }]} isReadOnly={true} onClick={tagChosen} />
                         </div>
-                        <div className='eventList'>
-                            {[...Array(10)].map((x, i) => <EventTile key={i} isLoading={true} />)}
+                        <div>
+                            <SearchField dictionary={search} maxSuggestions={6} onChosen={onSearch} />
                         </div>
                     </div>
-                </div>
-            </>
-        )
-    else
-        return (
-            <>
-                <div className='wholePageLayout navbarMargin'>
-                    <div className='eventsPage'>
-                        <div className='eventsNav'>
-                            <div>
-                                <TagList tags={[{ name: "Concert" }, { name: "Workshops" }, { name: "Conference" }, { name: "xyz" }, { name: "abc" }, { name: "123" }]} isReadOnly={true} onClick={tagChosen} />
-                            </div>
-                            <div>
-                                <SearchField dictionary={mockWordList} maxSuggestions={6} onChosen={onSearch} />
-                            </div>
-                        </div>
-                        <div className='eventList'>
-                            {data.map((e, i) => <EventTile key={i} data={e} isLoading={isLoading} />)}
-                        </div>
+                    <div className='eventList'>
+                        {events.map((e, i) => <EventTile key={i} data={e} isLoading={isLoading} />)}
                     </div>
                 </div>
-            </>
-        )
+            </div>
+        </>
+    )
 }
