4d52454cfa4e03ef641c1222fad365abe4ba5937
Emilia Starczyk <emiliastarczyk@gmail.com>
2022-05-21 13:25:10 +0200

    Fully working cancel and sending info about profile

--- frontend/src/Components/Input/SimpleEditableInput.tsx
+++ frontend/src/Components/Input/SimpleEditableInput.tsx
@@ -19,13 +19,14 @@ type SimpleEditableInputProps = {
     validationAction?: (value: string) => string,
     onKeyDownAction?: (event: React.KeyboardEvent<HTMLTextAreaElement>, currentValue: string) => void,
     isLoading?: boolean,
-    isNumber?: boolean
+    isNumber?: boolean,
+    delayTime?: number,
 }
 export default function SimpleEditableInput(props: SimpleEditableInputProps) {
     const [validationText, setValidationText] = React.useState("");
     const [text, setText] = React.useState<string>();
     const textAreaRef = useRef<HTMLTextAreaElement>(null);
-    const delayCallSetValues = useCallback(debounce(value => callChangeAction(value), 2000), []);
+    const delayCallSetValues = useCallback(debounce(value => callChangeAction(value), props.delayTime ? props.delayTime : 2000), []);
 
     React.useEffect(() => {
         if (props.defaultValue && textAreaRef.current.value !== props.defaultValue) {
--- frontend/src/Components/UserDataPage/EditableProfileSection.tsx
+++ frontend/src/Components/UserDataPage/EditableProfileSection.tsx
@@ -1,35 +1,45 @@
-import { ErrorMsg } from 'Components/Input/ErrorMsg';
 import { MenuButton } from 'Components/Input/MenuButton';
+import SimpleEditableInput from 'Components/Input/SimpleEditableInput';
 import * as React from 'react';
 import { Person } from 'react-bootstrap-icons';
 import { useNavigate } from 'react-router-dom';
 import ZiwgSkeleton from 'Utils/Skeletons';
 import { RootState, useAppDispatch, useAppSelector } from 'Utils/Store';
-import { EditableUserData, updateUserThunk, userApi } from 'Utils/UserApiSlice';
+import { updateUserThunk, userApi } from 'Utils/UserApiSlice';
 import "./EditableProfileSection.scss"
-import ToggleTextarea from './ToggleTextarea';
 
-
+const reducer = (state, action) => {
+    if (action.type === "set") {
+        return action.data;
+    }
+    const result = { ...state };
+    result[action.type] = action.value;
+    return result;
+};
 
 export function EditableProfileSection(props: any) {
-
     const data = useAppSelector((state: RootState) => state.userLogin.userData);
     const [changeDataRequest, response] = userApi.useChangeUserDataMutation()
     const [deleteUserRequest] = userApi.useDeleteUserMutation()
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
 
-    const [user, setUser]: [EditableUserData, any] = React.useState(
+    const [user, setUser] = React.useReducer(reducer,
         {
             displayName: data.displayName,
             description: data.description,
             location: data.location,
-        })
+        });
 
-    const [disabled, setEnabled]: [boolean, any] = React.useState(true)
+    const [disabled, setEnabled] = React.useState(true);
 
     const onToggle = () => setEnabled(!disabled)
-    const onChange = (e: any) => setUser({ ...user, [e.target.name]: e.target.value })
+
+    const onChange = (name: string, value: any) => {
+        console.log(`name: ${name} val: ${value}`);
+        setUser({ type: name, value })
+    }
+
     const onSave = () => {
         changeDataRequest(user).unwrap()
             .then(
@@ -40,6 +50,7 @@ export function EditableProfileSection(props: any) {
             )
             .catch((err) => { console.error(err); console.log(response); })
     }
+
     const onDelete = () => {
         if (window.confirm("Are you sure to delete account?")) {
             deleteUserRequest().unwrap()
@@ -49,13 +60,9 @@ export function EditableProfileSection(props: any) {
     }
 
     const onCancel = () => {
-        setUser(
-            {
-                displayName: data.displayName,
-                description: data.description,
-                location: data.location,
-            })
         onToggle();
+        setUser({ type: 'set', data });
+
     }
     const wip = () => alert("Work in progress")
 
@@ -68,26 +75,18 @@ export function EditableProfileSection(props: any) {
             </div>
             <div className='rightColumn'>
                 <div className='profileData'>
-                    <label>Name</label>
                     <span>
-                        {!user ? <ZiwgSkeleton /> :
-                            <input type="text" disabled={disabled} value={user.displayName ?? ""} maxLength={25} name="displayName" onChange={onChange} />}
+                        {!user ? <ZiwgSkeleton /> : <SimpleEditableInput inputClassName='userDisplayNameInputBox' id={'displayName'} inputDescription='Name' defaultValue={user.displayName ?? ""} onChangeAction={(id, value) => onChange(id, value)} isReadOnly={disabled} maxChars={25} delayTime={1000} />}
                     </span>
                 </div>
                 <div className='profileData'>
-                    <label>Location</label>
                     <span>
-                        {!user ? <ZiwgSkeleton /> :
-                            <input type="text" disabled={disabled} value={user.location ?? ""} maxLength={25} name="location" onChange={onChange} />}
+                        {!user ? <ZiwgSkeleton /> : <SimpleEditableInput inputClassName='userLocInputBox' id={'location'} inputDescription='Location' defaultValue={user.location ?? ""} onChangeAction={(id, value) => onChange(id, value)} isReadOnly={disabled} maxChars={25} delayTime={1000} />}
                     </span>
                 </div>
-
-                <hr />
-
                 <div className='profileData'>
-                    <label>Description</label>
                     <span>
-                        {!user ? <ZiwgSkeleton /> : <ToggleTextarea disabled={disabled} value={user.description ?? ""} maxLength={150} name="description" onChange={onChange} />}
+                        {!user ? <ZiwgSkeleton /> : <SimpleEditableInput inputClassName='userDescInputBox' id={'description'} inputDescription='Description' defaultValue={user.description ?? ""} onChangeAction={(id, value) => onChange(id, value)} isReadOnly={disabled} maxChars={150} delayTime={1000} />}
                     </span>
                 </div>
 
